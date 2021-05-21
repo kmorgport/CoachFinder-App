9cd4a1ffa03bc3e221c9551fd06c34dbeec6dbb0
kmorgport <kmorgan0057@gmail.com>
2021-05-21 17:50:12 -0500

    dummy data to establish routing

---
+++ src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,6 @@
+<template>
+Coaches
+<!-- child route requires its own router view -->
+<router-view></router-view>
+<router-link to="/coaches/c1/contact">Contact</router-link>
+</template>
--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import CoachRegistration from './pages/coaches/CoachRegister.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue'
@@ -25,3 +25,5 @@ const router = createRouter({
 
     ]
 })
+
+export default router
