13eb657d8d2495726d742d4a608a8aaa97e40303
kmorgport <kmorgan0057@gmail.com>
2021-05-22 20:23:04 -0500

    coach item compenent build out

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -8,16 +8,25 @@
             <router-link to="/register">Register a Coach</router-link>
         </div>
         <ul>
-            <li v-for="coach in filteredCoaches" :key=coach.id>
-                {{ coach.firstName }}
-            </li>
-        </ul>
+            <coach-item v-for="coach in filteredCoaches" :key="coach.id"
+            :id="coach.id"
+            :first-name="coach.firstName"
+            :last-name="coach.lastName"
+            :rate="coach.hourlyRate"
+            :areas="coach.areas"
+            ></coach-item>
+         </ul>
     </section>
 
 </template>
 
 <script>
+import CoachItem from "../../components/layout/coaches/CoachItem.vue"
+
 export default {
+    components: { 
+        CoachItem 
+        },
     computed:{
         filteredCoaches(){
             return this.$store.getters['coaches/coaches']
