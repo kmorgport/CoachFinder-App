cb94649b59e60f7975d5371e5a9b66a99c1c18dc
kmorgport <kmorgan0057@gmail.com>
2021-05-25 15:48:27 -0500

    adding force refresh

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -24,7 +24,10 @@ export default {
             id: userId
         });
     },
-    async loadCoaches(context){
+    async loadCoaches(context, payload){
+        if(!payload.forceRefresh && !context.getters.shouldUpdate){
+            return;
+        }
         const response = await fetch(
         `https://coachfinder-77b3c-default-rtdb.firebaseio.com/coaches.json`
         );
@@ -49,5 +52,6 @@ export default {
             coaches.push(coach);
         }
         context.commit('setCoaches', coaches)
+        context.commit('setFetchTimestamp')
     }
 };
