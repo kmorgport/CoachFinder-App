11795f4100a9ad4cece440e9d3eacb3422424752
kmorgport <kmorgan0057@gmail.com>
2021-05-25 16:24:45 -0500

    page not found page added

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+    <section>
+        <base-card>
+        <h2>Page Not Found</h2>
+        <p>This Page Could Not Be Found - Maybe check out some of our 
+            <router-link to="/coaches">coaches</router-link> </p>
+        </base-card>
+    </section>
+</template>
