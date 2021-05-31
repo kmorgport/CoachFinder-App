b224569bcb2258832884e48cabb74e304e91eda1
kmorgport <kmorgan0057@gmail.com>
2021-05-30 21:09:28 -0500

    request ading

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -86,6 +86,37 @@ export default {
            token: null,
            userId: null,
        })
-    }
-
+    },
+    async fetchRequests(context) {
+        const coachId = context.rootGetters.userId;
+        const token = context.rootGetters.token;
+        const response = await fetch(
+          `https://vue-http-demo-85e9e.firebaseio.com/requests/${coachId}.json?auth=` +
+            token
+        );
+        const responseData = await response.json();
+    
+        if (!response.ok) {
+          const error = new Error(
+            responseData.message || 'Failed to fetch requests.'
+          );
+          throw error;
+        }
+    
+        const requests = [];
+    
+        for (const key in responseData) {
+          const request = {
+            id: key,
+            coachId: coachId,
+            userEmail: responseData[key].userEmail,
+            message: responseData[key].message
+          };
+          requests.push(request);
+        }
+    
+        context.commit('setRequests', requests);
+      }
+    };
+    
 }
