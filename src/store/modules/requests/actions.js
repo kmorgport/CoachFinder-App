e7a33c01bfc07f223e8332d0078654567fe8ac78
kmorgport <kmorgan0057@gmail.com>
2021-06-01 23:57:04 -0500

    request display fixed

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -20,11 +20,36 @@ export default {
         }
 
         newRequest.id = responseData.name
+        newRequest.coachId = payload.coachId;
         context.commit('addRequest', newRequest)
     },
     async fetchRequests(context){
         const coachId = context.rootGetters.userId;
         const token = context.rootGetters.token;
-        await fetch(`https://coachfinder-77b3c-default-rtdb.firebaseio.com/requests/${coachId}.json?auth=${token}`)
+        const response = await fetch(
+            `https://coachfinder-77b3c-default-rtdb.firebaseio.com/requests/${coachId}.json?auth=${token}`
+            );
+        const responseData = await response.json();
+
+        if(!response.ok){
+            const error = new Error(
+                responseData.message || 'Failed to fetch requests.'
+            );
+            throw error
+        }
+
+        const requests = [];
+
+        for (const key in responseData){
+            const request = {
+                id : key,
+                coachid: coachId,
+                userEmail: responseData[key].userEmail,
+                message: responseData[key].message
+            };
+            requests.push(request);
+        }
+
+        context.commit('setRequests', requests)
     }
-}
+};
