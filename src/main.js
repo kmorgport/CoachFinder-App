c5f6b149c2695c7fb9b1a5e81495d6cbb92068c2
kmorgport <kmorgan0057@gmail.com>
2021-06-03 20:36:08 -0500

    integrating async components

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue';
+import { createApp, defineAsyncComponent } from 'vue';
 import router from './router.js'
 import App from './App.vue';
 import store from './store/index.js'
@@ -6,8 +6,11 @@ import BaseBadge from './components/ui/BaseBadge.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseSpinner from './components/ui/BaseSpinner.vue';
-import BaseDialog from './components/ui/BaseDialog.vue';
+// import BaseDialog from './components/ui/BaseDialog.vue';
 
+const BaseDialog = defineAsyncComponent(()=>{
+    import('./components/ui/BaseDialog.vue')
+})
 
 const app = createApp(App)
 
